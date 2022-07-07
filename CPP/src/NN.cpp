473c8309ce94cc82318a4dfb298f91cf173828b1
taliBerman5 <taliberman5@gmail.com>
2022-07-07 19:52:55 +0300

    init

---
+++ CPP/src/NN.cpp
@@ -0,0 +1,5 @@
+//
+// Created by talib on 7/7/2022.
+//
+
+#include "despot/NN.h"
---
+++ CPP/src/python_boost.cpp
@@ -0,0 +1,16 @@
+//
+// Created by talib on 7/7/2022.
+//
+
+
+
+
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(pomcp)
+        {
+
+                // Add regular functions to the module.
+                def("greet", greet);
+                def("square", square);
+        };
