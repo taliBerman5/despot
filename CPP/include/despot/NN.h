136201b249cf73ebb3dfe0fbbdc46d9046a92ac9
Lab-Antec <plpbgu@post.bgu.ac.il>
2022-07-14 14:34:51 +0300

    changed net

--- CPP/include/despot/NN.h
+++ CPP/include/despot/NN.h
@@ -7,30 +7,39 @@
 
 
 #include <torch/torch.h>
-//#include <torch.h>
-class NN {
 
-//    struct Net : torch::nn::Module {
-//        Net() {
-//            // Construct and register two Linear submodules.
-//            fc1 = register_module("fc1", torch::nn::Linear(784, 64));
-//            fc2 = register_module("fc2", torch::nn::Linear(64, 32));
-//            fc3 = register_module("fc3", torch::nn::Linear(32, 10));
-//        }
-//
-//        // Implement the Net's algorithm.
-//        torch::Tensor forward(torch::Tensor x) {
-//            // Use one of many tensor manipulation functions.
-//            x = torch::relu(fc1->forward(x.reshape({x.size(0), 784})));
-//            x = torch::dropout(x, /*p=*/0.5, /*train=*/is_training());
-//            x = torch::relu(fc2->forward(x));
-//            x = torch::log_softmax(fc3->forward(x), /*dim=*/1);
-//            return x;
-//        }
-//
-//        // Use one of many "standard library" modules.
-//        torch::nn::Linear fc1{nullptr}, fc2{nullptr}, fc3{nullptr};
-//    };
+class NN {
+protected:
+    static int nstates;
+    static int nactions;
+
+    struct Net : torch::nn::Module {
+        Net(int num_states, int num_actions) {
+            nstates = num_states;
+            nactions = num_actions;
+            // Construct and register two Linear submodules.
+            fc1 = register_module("fc1", torch::nn::Linear(nstates, nstates));
+            fc2 = register_module("fc2", torch::nn::Linear(nstates, nactions * 2));
+        }
+
+        // Implement the Net's algorithm.
+        torch::Tensor forward(torch::Tensor x) {
+            // Use one of many tensor manipulation functions.
+            x = torch::relu(fc1->forward(x));
+            x = fc2->forward(x);
+//            x = torch::nn::UnflattenImpl(1, (2, nactions));
+            return x;
+        }
+
+        // Use one of many "standard library" modules.
+        torch::nn::Linear fc1{nullptr}, fc2{nullptr};
+    };
+
+    /**
+ * output is what we predicted (v and n)
+ * target is what we get
+ */
+    virtual int bsa_loss(torch::Tensor output, torch::Tensor target);
 };
 
 
